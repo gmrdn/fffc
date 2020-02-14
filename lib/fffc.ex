83258f40dd3dd6a583259a1c477e89d0099f6888
Guillaume R <guillaume.rondon@gmail.com>
2020-02-14 19:42:16 +0100

    first commit 🎉

---
+++ lib/fffc.ex
@@ -0,0 +1,18 @@
+defmodule Fffc do
+  @moduledoc """
+  Documentation for Fffc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fffc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fffc_test.exs
@@ -0,0 +1,8 @@
+defmodule FffcTest do
+  use ExUnit.Case
+  doctest Fffc
+
+  test "greets the world" do
+    assert Fffc.hello() == :world
+  end
+end
